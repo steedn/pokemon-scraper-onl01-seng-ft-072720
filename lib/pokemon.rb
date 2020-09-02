3048b313df87f693072476242b6357e1f4ecda1b
Nathan Steed <steedn@outlook.com>
2020-09-02 04:37:35 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,7 +7,6 @@ class Pokemon
     @type = type
     @db = db
   end
-  def self.save(naem, type, db)
-    @@all << self
-  end
+  def self.save(name, type, db)
+db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)  end
 end
